86af76db1fd335250eed7e330d591374d2718423
Arindam Ganguly <gangulyarin@gmail.com>
2021-01-28 12:12:49 +0530

    Python Ip and Port changed 2

--- demo.py
+++ demo.py
@@ -9,5 +9,5 @@ def hello():
 if __name__ == "__main__": 
 	#port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
     	#ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-	app.run(host ='0.0.0.0', port = 6020, debug = True) 
+	app.run(host ='0.0.0.0', port = 8080, debug = True) 
 
